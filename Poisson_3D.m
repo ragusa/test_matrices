a588a2359ab3716d722689a654a6a8db3bb2063a
Gustavo Chávez <gchavez2@gmail.com>
2017-09-16 22:48:54 -0700

    Save matrices in matrix market coordinate format

--- Poisson_3D.m
+++ Poisson_3D.m
@@ -1,4 +1,6 @@
-n = 8;
+clc; clear all;
+
+n = 2;
 h = 1/(n+1);
 h2=(n+1)*(n+1);
 
@@ -9,3 +11,13 @@ K2D = kron(K1D,I1D)+kron(I1D,K1D);            % 2d Poisson matrix
 
 I2D = speye(size(K2D));                       % 2d identity matrix
 K3D = kron(K2D,I1D)+kron(I2D,K1D);            % 3d Poisson matrix
+
+return;
+
+% spy(K3D);
+
+FILENAME_MAT = strcat('K3D_n',int2str(n),'.mat')
+% save(FILENAME_MAT, 'K3D');
+
+FILENAME_MTX = strcat('K3D_n',int2str(n),'.mtx')
+mmwrite(FILENAME_MTX, K3D, 'GC: Saving K3D matrix')
