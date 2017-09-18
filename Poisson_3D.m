bdc9c00d35ddd3d1a45e2c188297f425277b4f28
Gustavo Chávez <gchavez2@gmail.com>
2017-09-17 20:16:01 -0700

    Put a loop for 3 sizes

--- Poisson_3D.m
+++ Poisson_3D.m
@@ -1,23 +1,35 @@
-clc; clear all;
+clc; clear;
 
-n = 2;
-h = 1/(n+1);
-h2=(n+1)*(n+1);
 
-K1D = spdiags(ones(n,1)*[-1 2 -1],-1:1,n,n);  % 1d Poisson matrix
+sizes = [32,64,128]
 
-I1D = speye(size(K1D));                       % 1d identity matrix
-K2D = kron(K1D,I1D)+kron(I1D,K1D);            % 2d Poisson matrix
+for i = 1:3
 
-I2D = speye(size(K2D));                       % 2d identity matrix
-K3D = kron(K2D,I1D)+kron(I2D,K1D);            % 3d Poisson matrix
+	tic;
+	
+	n = sizes(i);
 
-return;
+	fprintf('n = %d \n', n )
 
-% spy(K3D);
+	% h = 1/(n+1);
+	% h2=(n+1)*(n+1);
 
-FILENAME_MAT = strcat('K3D_n',int2str(n),'.mat')
-% save(FILENAME_MAT, 'K3D');
+	K1D = spdiags(ones(n,1)*[-1 2 -1],-1:1,n,n);  % 1d Poisson matrix
 
-FILENAME_MTX = strcat('K3D_n',int2str(n),'.mtx')
-mmwrite(FILENAME_MTX, K3D, 'GC: Saving K3D matrix')
+	I1D = speye(size(K1D));                       % 1d identity matrix
+	K2D = kron(K1D,I1D)+kron(I1D,K1D);            % 2d Poisson matrix
+
+	I2D = speye(size(K2D));                       % 2d identity matrix
+	K3D = kron(K2D,I1D)+kron(I2D,K1D);            % 3d Poisson matrix
+
+	% spy(K3D);
+	 
+	% FILENAME_MAT = strcat('K3D_n',int2str(n),'.mat')
+	% save(FILENAME_MAT, 'K3D');
+
+	FILENAME_MTX = strcat('K3D_n',int2str(n),'.mtx');
+	mmwrite(FILENAME_MTX, K3D, 'GC: Saving K3D matrix')
+
+	toc
+
+end
